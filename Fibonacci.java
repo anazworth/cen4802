c4cb561ab84dbbe14bb50030f2e7ee3eb7e64d38
anazworth <98629900+anazworth@users.noreply.github.com>
2023-08-29 18:30:48 -0400

    Added Javadoc to Fibonacci class per issue 1

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,7 +6,17 @@ public class Fibonacci {
         System.out.println("The " + N + "th Fibonacci number is " + fib + ".");
     }
 
+    /**
+     * Returns the nth Fibonacci number using recursion.
+     * 
+     * @precondition n >= 0
+     * @param n, the nth Fibonacci number to return
+     * @return the nth Fibonacci number (0, 1, 1, 2, 3, 5, 8, 13, 21, ...)
+     */
     public static int fibonacci(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be >= 0");
+        }
         if (n == 0 || n == 1) {
             return n;
         }
